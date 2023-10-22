7651b6e1a56474b07189fc546a1770688cc88daf
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2023-10-22 10:14:24 -0700

    Time: 1 ms (99.40%), Space: 44 MB (87.58%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xy = 0, x = 0, y = 0, b = 1;
+        
+        for(int n : nums){
+            xy ^= n;
+        }
+        
+        while((xy & b) == 0)
+            b = b << 1;
+        
+        for(int n : nums){
+            if((n & b) != 0)
+                x ^= n;
+            else
+                y ^= n;
+        }
+        
+        return new int[] {x,y};
+        
+    }
+}
