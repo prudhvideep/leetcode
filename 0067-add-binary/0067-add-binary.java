65c92589b510dc9eb0f074e69ff3a541f6ec9d44
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2023-11-04 23:48:43 -0700

    Time: 2 ms (55.45%), Space: 41.7 MB (27.48%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1, j = b.length()-1, s = 0, c = 0;
+        StringBuffer sb = new StringBuffer();
+        
+        while(i >= 0 && j >= 0){
+            int x = (a.charAt(i) == '0') ? 0 : 1;
+            int y = (b.charAt(j) == '0') ? 0 : 1;
+                        
+            s = (x+y+c)%2;
+            c = (x+y+c)/2;
+            
+            sb.insert(0,(s == 1)?'1':'0');
+            i--;
+            j--;
+        }
+        
+        while(i >= 0){
+            int x = (a.charAt(i) == '0') ? 0 : 1;
+            s = (x+c)%2;
+            c = (x+c)/2;
+            sb.insert(0,(s == 1)?'1':'0');
+            i--;
+        }
+        while(j >= 0){
+            int y = (b.charAt(j) == '0') ? 0 : 1;
+            s = (y+c)%2;
+            c = (y+c)/2;
+            sb.insert(0,(s == 1)?'1':'0');
+            j--;
+        }
+        if(c != 0)
+            sb.insert(0,(c == 1)?'1':'0');
+        
+        return sb.toString();
+    }
+}
