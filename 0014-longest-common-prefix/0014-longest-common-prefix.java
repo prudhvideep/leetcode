584b51dff1d9a6be9998c465752a55bdc49518b0
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2023-12-15 16:09:49 -0500

    Update 0014-longest-common-prefix.java

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -8,11 +8,6 @@ class Solution {
         for(int i = 0;i < strs[0].length();i++){
             char c = strs[0].charAt(i);
             for(int j = 0;j < strs.length;j++){
-               // if(strs[j].length() == 0)
-               //     return "";
-                
-               // System.out.println("Strlen : " + strs[j].length());
-               // System.out.println("index : " + i);
                 
                 if(i >= strs[j].length())
                     return sb.toString();
