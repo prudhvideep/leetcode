716af83f6e20f5a68b63e8560818b71b18810584
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-01-04 16:25:29 -0500

    Time: 18 ms (73.74%), Space: 42.4 MB (67.54%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public String longestPalindrome(String s) {
+        int maxOddLen = 1, maxEvenLen = 0;
+        int oddStId = 0, oddEnId = 0, evenStId = 0, evenEnId = 0;
+        
+        for(int i = 0;i < s.length();i++){
+            
+            int l = i-1, h = i+1, len = 1;
+            
+            while((l >= 0 && h < s.length()) && (s.charAt(l) == s.charAt(h))){
+                len+=2;
+                if(len > maxOddLen){
+                    maxOddLen = len;
+                    oddStId = l;
+                    oddEnId = h;
+                }
+                l--;h++;
+            }
+        }
+                  
+        for(int i = 0;i < s.length()-1;i++){
+            int l = i, h = i+1, len = 0;
+            
+            while((l >= 0 && h < s.length()) && (s.charAt(l) == s.charAt(h))){
+                len+=2;
+                if(len > maxEvenLen){
+                    maxEvenLen = len;
+                    evenStId = l;
+                    evenEnId = h;
+                }
+                l--;h++;
+            }
+        }
+        
+        //System.out.println("oddStId : " +  oddStId + " oddEnId : " + oddEnId);
+        //System.out.println("evenStId : " +  evenStId + " evenEnId : " + evenEnId);
+        
+        if(maxOddLen > maxEvenLen)
+            return s.substring(oddStId,oddEnId+1);
+        
+        return s.substring(evenStId,evenEnId+1);
+    }
+}
