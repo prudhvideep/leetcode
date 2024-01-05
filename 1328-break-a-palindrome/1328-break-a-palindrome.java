90d2a05aba20c30181b1136be3d591f7712643ad
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-01-05 16:02:52 -0500

    Time: 0 ms (100.00%), Space: 41.7 MB (5.17%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,29 +1,17 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
-        if(palindrome.length() == 1)
-            return "";
+        char [] chAr = palindrome.toCharArray();
+        int len = palindrome.length();
         
-        
-        int id = 0;
-        while(id < palindrome.length() && palindrome.charAt(id) == 'a'){
-            id++;
+        for(int i = 0;i < len/2;i++){
+            char c = chAr[i];
+            if(c != 'a'){
+                chAr[i] = 'a';
+                return String.valueOf(chAr);
+            }
         }
         
-        StringBuffer sb = new StringBuffer(palindrome);
-        
-        if(id == palindrome.length()){
-            sb.setCharAt(id-1,'b');
-            return sb.toString();
-        }
-        
-        
-        if((id-1 >= 0 && sb.charAt(id-1) == 'a') && (id+1 < sb.length() && sb.charAt(id+1) == 'a')){
-            sb.setCharAt(sb.length()-1,'b');
-            return sb.toString();
-        }
-    
-        sb.setCharAt(id,'a');
-        
-        return sb.toString();
+        chAr[len-1] = 'b';
+        return (len == 1)? "" : String.valueOf(chAr);   
     }
 }
