74d4d0fdd8d1f2601a7c44b6b7b148e2d91ad02b
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-02-13 12:30:44 -0700

    Time: 9 ms (90.87%), Space: 44.8 MB (56.45%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,50 @@
 class Solution {
-    public boolean compare(int [] pc, int [] sc){
+    public boolean compare (int []pc,int []sc){
         for(int i = 0;i < 26;i++){
             if(pc[i] != sc[i])
                 return false;
         }
         return true;
-    }    
+    }
+    
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
+        List <Integer> list = new ArrayList<>();
+        
         int sl = s.length(), pl = p.length();
         
-        int [] pc = new int[26];
-        int [] sc = new int [26];
+        if(pl > sl)
+            return list;
+        
+        int sc[] = new int[26];
+        int pc[] = new int[26];
         
         for(int i = 0;i < pl;i++){
             char c = p.charAt(i);
-            pc[c - 'a']++;
+            pc[c-'a']++;
+            
+            char d = s.charAt(i);
+            sc[d-'a']++;
         }
         
-        for(int i = 0;i < sl-pl+1; i++){
-            Arrays.fill(sc,0);
-            for(int j = i;j < pl+i;j++){
-                char c = s.charAt(j);
-                int id = c-'a';
-                
-                if(pc[id] == 0){
-                    break;
-                }
-                sc[id]++;
+        if(compare(pc,sc)){
+            list.add(0);
+        }
+        
+        int left = 0, right = pl;
+        
+        while(right < sl){
+            char lc = s.charAt(left);
+            char rc = s.charAt(right);
+            
+            sc[lc-'a']--;
+            sc[rc-'a']++;
+            
+            if(compare(pc,sc)){
+                list.add(left+1);
             }
             
-            if(compare(pc,sc))
-                list.add(i);
+            left++;
+            right++;
         }
         
         return list;
