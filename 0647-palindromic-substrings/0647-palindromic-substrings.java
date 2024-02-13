eb71cf14cec6f387eb228d8e3011ae6039cc588b
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-02-13 10:09:15 -0700

    Time: 3 ms (70.75%), Space: 41.5 MB (55.27%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0, n = s.length();
+        char [] chAr = s.toCharArray();
+        
+        //Odd number palindromes
+        for(int i = 0;i < n;i++){
+            int j = i;
+            int k = i;
+            
+            while(j>=0 && k<n && (s.charAt(j) == s.charAt(k))){
+                count++;
+                j--;
+                k++;
+            }
+        }
+        
+        //Even number palindromes
+        for(int i = 0;i < n;i++){
+            int j = i;
+            int k = i+1;
+            
+            while(j>=0 && k < n && (s.charAt(j) == s.charAt(k))){
+                count++;
+                j--;
+                k++;
+            }
+        }
+        
+        return count;
+    }
+}
