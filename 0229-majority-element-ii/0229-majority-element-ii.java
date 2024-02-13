ba240717b91dd8eeb48af17028d4cfd02aa3f12f
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-02-12 22:49:04 -0700

    Time: 2 ms (99.46%), Space: 48.1 MB (39.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,13 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
+        List <Integer> list = new ArrayList<Integer>();
         
-        List <Integer> list = new ArrayList<>();
+        int m1 = 0, m2 = 0, n1 = 0, n2 = 0, n = nums.length;
         
-        if(n == 1)
-        {
-            list.add(nums[0]);
-            return list;   
-        }
-        
-        int m1 = 0, m2 = 0,n1 = 0,n2 = 0;
-        for(int i = 0;i < nums.length;i++){
-            if(m1 == nums[i]){
+        for(int i = 0;i < n;i++){
+            if(nums[i] == m1){
                 n1++;
-            }else if(m2 == nums[i]){
+            }else if(nums[i] == m2){
                 n2++;
             }else if(n1 == 0){
                 m1 = nums[i];
@@ -28,19 +21,22 @@ class Solution {
             }
         }
         
-        int rf = n/3,count1 = 0,count2 = 0;
-        for(int i = 0;i < n;i++){
+        int rf = n/3, count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++){
             if(nums[i] == m1){
                 count1++;
             }
+            
             if(nums[i] == m2){
                 count2++;
-            } 
+            }
         }
+        
         if(count1 > rf){
             list.add(m1);
-            if(m1 == m2)
+            if(m1 == m2){
                 return list;
+            }
         }
         if(count2 > rf){
             list.add(m2);
