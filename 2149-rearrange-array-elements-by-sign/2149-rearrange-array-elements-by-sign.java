2e97a30a3ba5ef967905b760b5ac8f0ebd44a050
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-02-15 11:42:03 -0700

    Time: 3 ms (100.00%), Space: 76.4 MB (86.11%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,31 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length, pi = 0, ni = 0;
-        
-        int [] pa = new int[n/2];
-        int [] na = new int[n/2];
+        int n = nums.length, p = 0, ni = 1;
+        int [] res = new int[n];
         
         for(int i = 0;i < n;i++){
             if(nums[i] >= 0){
-                pa[pi] = nums[i];
-                pi++;
+                res[p] = nums[i];
+                p+=2;
             }else{
-                na[ni] = nums[i];
-                ni++;
+                res[ni] = nums[i];
+                ni+=2;
             }
         }
         
-        pi = 0;ni = 0;
-        for(int i = 0;i < n;i++){
-            if(i%2 == 0){
-                nums[i] = pa[pi];
-                pi++;
-            }else{
-                nums[i] = na[ni];
-                ni++;
-            }
-        }
-        
-        return nums;
+        return res;
     }
 }
