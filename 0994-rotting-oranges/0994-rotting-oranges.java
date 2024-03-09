582562c00ab1917d4b7c4798b7e876fa5cd4fb02
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-03-08 23:48:36 -0700

    Time: 3 ms (17.75%), Space: 42.8 MB (14.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,9 +2,6 @@ class Solution {
     public int time = -1,r = 0,c = 0;
     
     public int orangesRotting(int[][] grid) {
-        if(grid == null)
-            return -1;
-        
         r = grid.length; c = grid[0].length;
         int maxTime = Integer.MIN_VALUE;
         int fo = getFreshOrangeCount(grid);
