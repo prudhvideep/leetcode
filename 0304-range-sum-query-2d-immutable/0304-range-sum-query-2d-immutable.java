ee59adca3d3daac62e68e9eb043bf7679112b5aa
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-03-10 12:19:17 -0600

    Time: 105 ms (39.16%), Space: 65.1 MB (98.45%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,25 +1,31 @@
 class NumMatrix {
     public int [][] matrix;
-    public int [][] prefix;
     public NumMatrix(int[][] matrix) {
         this.matrix = matrix;
-        prefix = new int[matrix.length][matrix[0].length];
         
         for(int i = 0;i < matrix.length;i++){
-            for(int j = 0;j < matrix[0].length;j++){
-                prefix[i][j] = (j == 0)? matrix[i][0]: (prefix[i][j-1] + matrix[i][j]);
+            for(int j = 1;j < matrix[0].length;j++){
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+
+        for(int j = 0;j < matrix[0].length;j++){
+            for(int i = 1;i < matrix.length;i++){
+                matrix[i][j] += matrix[i-1][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        
-        for(int i = row1; i <= row2; i++){
-           sum += (prefix[i][col2] - prefix[i][col1]) + matrix[i][col1];
+        if(row1 == 0 && col1 == 0){
+            return matrix[row2][col2];
+        }else if(row1 == 0 && col1 != 0){
+            return matrix[row2][col2] - matrix[row2][col1-1];
+        }else if(row1 != 0 && col1 ==0) {
+            return matrix[row2][col2] - matrix[row1-1][col2];
         }
         
-        return sum;
+        return matrix[row2][col2] - matrix[row2][col1-1] - matrix[row1-1][col2] + matrix[row1-1][col1-1];
     }
 }
 
