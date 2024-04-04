30f48f3aefa3ca570b22259428e73db92175a304
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-04 10:50:36 -0700

    Time: 8 ms (63.12%), Space: 56.7 MB (65.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -9,35 +9,20 @@ class Solution {
     
     void partUtil(List <String> list, int id, String s){
         if(id == s.length()){
-            if(isValid(list)){
-                part.add(new ArrayList<String> (list));
-            }
+            part.add(new ArrayList<String> (list));
             return;
         }
         
         for(int i = id;i < s.length();i++){
-            String subStr = s.substring(id,i+1);
-            list.add(subStr);
-            partUtil(list,i+1,s);
-            list.remove(list.size()-1);
+            if(isPali(s,id,i)){
+                list.add(s.substring(id,i+1));
+                partUtil(list,i+1,s);
+                list.remove(list.size()-1);   
+            }
         }
     }
     
-    boolean isValid(List <String> list){
-        for(String str : list){
-            if(str.length() == 1)
-                continue;
-            
-            if(!isPali(str))
-                return false;
-        }
-        
-        return true;
-    }
-    
-    boolean isPali(String s){
-        int l = 0, h = s.length()-1;
-        
+    boolean isPali(String s,int l,int h){        
         while(l < h){
             if(s.charAt(l) != s.charAt(h))
                 return false;
