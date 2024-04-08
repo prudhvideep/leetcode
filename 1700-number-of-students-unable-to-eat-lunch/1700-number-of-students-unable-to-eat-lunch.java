2fdb887643703c23dcad39c16f0d07f3d309534e
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-08 13:39:07 -0700

    Time: 1 ms (70.71%), Space: 41.2 MB (83.96%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,41 +1,22 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        int c0 = 0, c1 = 0, sl = students.length, swl = sandwiches.length;
+        int [] pref = new int[2];
         
-        for(int i = 0;i < students.length;i++){
-            if(students[i] == 1)
-                c1++;
-            
-            if(students[i] == 0)
-                c0++;
+        for(int i : students){
+            pref[i]++;
         }
-                
-        int sId = 0, wId = 0, count = 0;
         
-        while(true){
-            count = 0;
-            
-            while(count <= sl && students[sId] != sandwiches[wId]){
-                sId = (sId+1)%sl;
-                count++;  
-            }
-            
-            if(count >= sl)
+        for(int sandwich : sandwiches){
+            if(pref[sandwich] == 0)
                 break;
             
-            if(students[sId] == sandwiches[wId]){
-                if(students[sId] == 1){
-                    c1--;
-                }else{
-                    c0--;
-                }
-                
-                students[sId] = -1;
-                sId = (sId+1)%sl;
-                wId = (wId+1)%swl;
-            }            
+            pref[sandwich]--;
         }
         
-        return (c1+c0);
+        
+        int count = Arrays.stream(pref).sum();
+        return count;
+        
+        
     }
 }
