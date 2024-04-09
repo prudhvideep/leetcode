3051f8bf83a21ae40cc50c2c46bf0ee51b4dbd86
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-09 13:20:11 -0700

    Time: 1 ms (70.99%), Space: 46 MB (55.73%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        
+        int [] nge = getNge(height,n);
+        int [] pge = getPge(height,n);
+        
+        
+        int water = 0;
+        for(int i = 0;i < n;i++){
+           int amount = Math.min(pge[i],nge[i]) - height[i];
+           //System.out.println(i + " " + pge[i] + " " + nge[i]);
+           water+= amount;
+        }
+        
+        return water;
+    }
+    
+    
+    public int [] getPge(int [] height,int n){
+        int [] pge = new int [n];
+        pge[0] = height[0];
+        
+        for(int i = 1;i < n;i++){
+            pge[i] = Math.max(height[i],pge[i-1]);
+        }
+        
+        return pge;
+    }
+    
+    public int [] getNge(int [] height,int n){
+        
+        int [] nge = new int [n];
+        nge[n-1] = height[n-1];
+        
+        for(int i = n-2;i >= 0; i--){
+            nge[i] = Math.max(height[i],nge[i+1]);
+        }
+        
+        return nge;
+    }
+}
