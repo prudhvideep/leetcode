ac35a416ba0f9eec4f43a09cfe2be37a7e022a1b
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-12 14:18:00 -0700

    Time: 1 ms (61.05%), Space: 40.7 MB (12.01%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,4 +1,5 @@
 class Solution {
+    HashMap <Integer,Integer> hm = new HashMap<>();
     
     public int integerReplacement(int n) {
         if(n >= Integer.MAX_VALUE) return 32;
@@ -7,7 +8,18 @@ class Solution {
         if(n < 3) return 1;
         if(n < 5) return 2;
         
-        if(n%2 == 0) return 1+integerReplacement(n/2);
-        return 1+ Math.min(integerReplacement(n+1),integerReplacement(n-1));
+        if(n%2 == 0){
+            if(!hm.containsKey(n/2)){
+                hm.put(n/2,integerReplacement(n/2));
+            }
+            return 1+hm.get(n/2);
+        }
+        if(!hm.containsKey(n-1))
+            hm.put(n-1,integerReplacement(n-1));
+        
+        if(!hm.containsKey(n+1))
+            hm.put(n+1,integerReplacement(n+1));
+        
+        return 1+Math.min(hm.get(n-1),hm.get(n+1));
     }
 }
