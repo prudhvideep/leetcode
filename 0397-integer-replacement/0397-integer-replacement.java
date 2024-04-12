7f176ab7a0de7b90ee47e944f67eb6326c1651b6
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-12 14:09:39 -0700

    Time: 2 ms (40.70%), Space: 39.6 MB (98.39%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+    public int integerReplacement(int n) {
+        if(n >= Integer.MAX_VALUE) return 32;
+        
+        if(n == 1) return 0;
+        if(n < 3) return 1;
+        if(n < 5) return 2;
+        
+        if(n%2 == 0) return 1+integerReplacement(n/2);
+        return 1+ Math.min(integerReplacement(n+1),integerReplacement(n-1));
+    }
+}
