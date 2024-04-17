d47db8f1ea11c91233753c014a485f1e31fcc19b
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-16 22:24:43 -0700

    Time: 1 ms (99.37%), Space: 43.9 MB (86.58%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -32,35 +32,18 @@ class Solution {
         
         //System.out.println(sb);
         if(root.left == null && root.right == null){
-            checkLexi(sb);
+            if(retString == null || retString.length() == 0){
+                retString = sb.toString();
+            }
+            
+            if(retString.compareTo(sb.toString()) > 0){
+                retString = sb.toString();
+            }
         }
         
         helper(root.left,sb);
         helper(root.right,sb);
         
         sb.deleteCharAt(0);
-    }
-    
-    public void checkLexi(StringBuilder sb){
-        if(retString == null || retString.length() == 0){
-            retString = sb.toString();
-            return;
-        }
-        System.out.println(retString);
-        
-        String sbStr = sb.toString();
-        int len = Math.min(retString.length(),sbStr.length());
-        
-        for(int i = 0;i < len;i++){
-            if(retString.charAt(i) > sbStr.charAt(i)){
-                retString = sbStr;
-                return;
-            }else if(retString.charAt(i) < sbStr.charAt(i)){
-                return;
-            }
-        }
-        
-        if(retString.length() > sbStr.length())
-            retString = sbStr;
     }
 }
