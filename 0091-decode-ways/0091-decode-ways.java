a1dc31fc7245f3dfbdfc3717d7139f986950d4ba
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-17 23:21:10 -0700

    Time: 5 ms (8.11%), Space: 45.3 MB (5.37%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int dp[] = new int [n];
+        
+        String temp = s.substring(n-1,n);
+        int a  = Integer.valueOf(temp);
+                
+        if(a >= 1 && a <= 9){
+            dp[n-1] = 1;
+        }else{
+            dp[n-1] = 0;
+        }
+        
+        for(int i = n-2;i >=0 ;i--){
+            int sum = 0;
+            
+            for(int j = i; j < n;j++){
+                boolean isValid = false;
+                String prefix = s.substring(i,j+1);
+                
+                int prefix_int = -1;
+                
+                if(prefix.length() <= 4){
+                    prefix_int = Integer.valueOf(prefix);
+                }
+                
+                if(prefix_int == 0){
+                    dp[i] = 0;
+                    break;
+                }
+                    
+                if(prefix_int >= 1 && prefix_int <= 26){
+                    isValid = true;
+                }
+                
+                if(isValid){
+                    if(j == n-1){
+                        sum+=1;
+                    }else{
+                        sum+=dp[j+1];
+                    }
+                }
+            }
+            dp[i] = sum;
+        }
+        return dp[0];
+    }
+}
