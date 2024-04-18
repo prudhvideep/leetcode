7c94a4ba32d4d6920df5b7f75b92d2ada88df0b3
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-18 12:28:03 -0700

    Time: 4 ms (99.58%), Space: 45.8 MB (41.79%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,44 +1,31 @@
 class Solution {
     public int perimeter = 0;
     public int islandPerimeter(int[][] grid) {
-        int r = grid.length, c = grid[0].length;
-        
-        boolean visited[][] = new boolean [r][c];
-              
+        int r = grid.length, c = grid[0].length, p = 0;
+                  
         for(int i = 0;i < r;i++){
+            int sum = 0;
             for(int j = 0;j < c;j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
-                    dfs(i,j,visited,grid);
-                }
+                
+               if(grid[i][j] == 0) continue;
+                
+               sum+=4;
+               
+                if(i-1 >= 0 && grid[i-1][j] == 1)
+                   sum-=1;
+                
+                if(i+1 < r && grid[i+1][j] == 1)
+                   sum-=1;
+                
+                if(j-1 >= 0 && grid[i][j-1] == 1)
+                    sum-=1;
+                
+                if(j+1 < c && grid[i][j+1] == 1)
+                    sum-=1;
             }
+            p+=sum;
         }
         
-        return perimeter;
-    }
-    
-    public void dfs(int row,int col, boolean [][] visited, int [][] grid){
-        visited[row][col] = true;
-        int r = grid.length, c = grid[0].length;
-        
-        if(row-1 < 0 || (row-1 >= 0 && grid[row-1][col] == 0))perimeter++;
-        
-        if(row+1 >= r || (row+1 < r && grid[row+1][col] == 0))perimeter++;
-        
-        if(col-1 < 0 || (col-1 >= 0 && grid[row][col-1] == 0))perimeter++;
-           
-        if(col+1  >= c || (col+1 < c && grid[row][col+1] == 0))perimeter++;
-           
-        
-        if(row-1 >= 0 && !visited[row-1][col] && grid[row-1][col] == 1)
-           dfs(row-1,col,visited,grid);
-           
-        if(row+1 < r && !visited[row+1][col] && grid[row+1][col] == 1)
-           dfs(row+1,col,visited,grid);
-           
-        if(col-1 >= 0 && !visited[row][col-1] && grid[row][col-1] == 1)
-           dfs(row,col-1,visited,grid);
-           
-         if(col+1 < c && !visited[row][col+1] && grid[row][col+1] == 1)
-           dfs(row,col+1,visited,grid);
+        return p;
     }
 }
