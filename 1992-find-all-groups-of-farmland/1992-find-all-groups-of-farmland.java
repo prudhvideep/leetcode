266fb35f706e0516fbc8d12c22a0c62e3d76f014
Prudhvi <41674562+prudhvideep@users.noreply.github.com>
2024-04-20 14:59:59 -0700

    Time: 12 ms (59.55%), Space: 71.3 MB (20.97%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[][] findFarmland(int[][] land) {
+        List <int []> list = new ArrayList<>();
+        
+        for(int i = 0;i < land.length;i++){
+            for(int j = 0;j < land[0].length;j++){
+                if(land[i][j] == 1){
+                    int [] arr = new int[4];
+                    arr[0] = i;
+                    arr[1] = j;
+                    dfs(i,j,land,arr);
+                    list.add(arr);
+                }
+            }
+        }
+        
+        int [][] ret = new int[list.size()][];
+        
+        int id = 0;
+        for(int a [] : list){
+            ret[id] = a;
+            id++;
+        }
+        
+        return ret;
+    }
+    
+    public void dfs(int row,int col, int [][] land, int [] arr){
+        land[row][col] = 2;
+        
+        arr[2] = Math.max(arr[2],row);
+        arr[3] = Math.max(arr[3],col);
+        
+        if(row-1 >= 0 && land[row-1][col] == 1)
+            dfs(row-1,col,land,arr);
+        
+        if(row+1 < land.length && land[row+1][col] == 1)
+            dfs(row+1,col,land,arr);
+        
+        if(col-1 >= 0 && land[row][col-1] == 1)
+            dfs(row,col-1,land,arr);
+        
+        if(col+1 < land[0].length && land[row][col+1] == 1)
+            dfs(row,col+1,land,arr);
+    }
+}
