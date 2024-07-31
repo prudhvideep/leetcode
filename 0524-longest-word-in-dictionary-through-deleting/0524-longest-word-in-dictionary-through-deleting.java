f0d7f8f883aacf3edd2b36e6ea030b842a9b9ee8
Prudhvi <prudhvideep1996@gmail.com>
2024-07-30 23:14:34 -0700

    Time: 18 ms (54.97%), Space: 45.3 MB (32.83%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
@@ -1,36 +1,23 @@
 class Solution {
-    public String findLongestWord(String s, List<String> dictionary) {
-        
+    public boolean isSubSeq(String s, String dictStr){
         int i, j;
-        String retStr="";
-        for(String dictStr : dictionary){
-            i = 0; j = 0;
-            while(i < s.length() && j < dictStr.length()){
+         i = 0; j = 0;
+        while(i < s.length() && j < dictStr.length()){
                 if(s.charAt(i) == dictStr.charAt(j)){
                     j++;
                 }
                 i++;
-            }
-            if(j == dictStr.length()) {
-                
-                if(dictStr.length() > retStr.length()){
-                    retStr = dictStr;
-                }else if(dictStr.length() == retStr.length()){
-                    
-                    for(int k = 0;k < dictStr.length();k++){
-                        if(dictStr.charAt(k) == retStr.charAt(k))
-                            continue;
+        }
 
-                        if(dictStr.charAt(k) < retStr.charAt(k)){
-                            
-                            retStr = dictStr;
-                            break;
-                        }else if(dictStr.charAt(k) > retStr.charAt(k)){
-                            break;
-                        }
-                    }
-                }
-                
+        return j == dictStr.length();
+    }
+
+    public String findLongestWord(String s, List<String> dictionary) {
+        String retStr="";
+        for(String dictStr : dictionary){
+           if(isSubSeq(s,dictStr) && ((dictStr.length() > retStr.length()) 
+            || ( (dictStr.length() == retStr.length()) && (dictStr.compareTo(retStr) < 0)))){
+                retStr = dictStr;
             }
         }
 
