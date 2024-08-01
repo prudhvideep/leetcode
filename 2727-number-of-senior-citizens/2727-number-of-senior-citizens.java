d1adb500f0fe985e4be5d22e159e24bcc9fe1e5d
Prudhvi <prudhvideep1996@gmail.com>
2024-08-01 15:38:10 -0700

    Time: 4 ms (11.44%), Space: 42.6 MB (35.06%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -2,14 +2,10 @@ class Solution {
     public int countSeniors(String[] details) {
         int count = (int) Arrays.stream(details)
                           .filter((x) -> {
-                            int a = x.charAt(11) - '0';
-                            int b = x.charAt(12) - '0';
-
-                            a = (a * 10) + b;
-
-                            if(a > 60) return true;
-
-                            return false;
+                             if(Integer.parseInt(x.substring(11,13)) > 60){
+                                return true;
+                             }
+                             return false;
                           }).count();
         return count;
     }
