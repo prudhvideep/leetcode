3938663a1b630ca1a0640a18526365eeefb7cd99
Prudhvi <prudhvideep1996@gmail.com>
2024-08-11 23:18:06 -0700

    Time: 17 ms (32.66%), Space: 45.6 MB (54.89%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -2,20 +2,19 @@ class Solution {
     public int minimumPushes(String word) {
         int minPush = 0;
         int [] count = new int [26];
-        Set <Character> set = new HashSet<>();
 
         for(int i = 0;i < word.length();i++){
             count[word.charAt(i) - 'a']++;
-            set.add(word.charAt(i));
         } 
         
-        List <Character> list = set.stream().sorted((a,b) -> {
-            return count[b-'a'] - count[a-'a'];
-        }).toList();
+        List <Integer> list = Arrays.stream(count).boxed().sorted((a,b) -> (b-a)).toList();
 
         int ct = 0, fact = 1;
-        for(Character c : list){
-            minPush += count[c-'a']*fact;
+        for(int c : list){
+            if(c == 0)
+                continue;
+
+            minPush += c*fact;
 
             ct++;
             if(ct == 8){
