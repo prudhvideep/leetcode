88906c1a21e6da80ae4cf2d5fbc2fe2257dab589
Prudhvi <prudhvideep1996@gmail.com>
2024-08-18 00:04:24 -0700

    Time: 2 ms (91.97%), Space: 41.7 MB (46.15%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,17 +1,30 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int count = 0;Long uNum = 0L;
-        TreeSet <Long> set = new TreeSet<>((a,b) -> Long.compare(a,b));
-        set.add(1L);
+        int i2 = 1, i3 = 1, i5 = 1;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        while(count < n){
-            uNum = set.pollFirst();
-            count++;
+        for(int i = 2; i <= n;i++){
+            int nm2 = dp[i2] * 2;
+            int nm3 = dp[i3] * 3;
+            int nm5 = dp[i5] * 5;
 
-            set.add(uNum * 2);
-            set.add(uNum * 3);
-            set.add(uNum * 5);
+            dp[i] = Math.min(nm2,Math.min(nm3,nm5));
+
+            if(dp[i] == nm2){
+                i2++;
+            }
+
+            if(dp[i] == nm3){
+                i3++;
+            }
+
+            if(dp[i] == nm5){
+                i5++;
+            }
         }
-        return uNum.intValue();
+
+        return dp[n];
     }
 }
