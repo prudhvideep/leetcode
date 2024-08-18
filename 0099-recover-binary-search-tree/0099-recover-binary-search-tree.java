eab77155b0fd54ca6f87a52cff7a8bfb5ac3ae91
Prudhvi <prudhvideep1996@gmail.com>
2024-08-18 15:36:45 -0700

    Time: 1 ms (100%), Space: 44.3 MB (72.61%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,34 +14,36 @@
  * }
  */
 class Solution {
-    TreeNode prev = null,first = null, second = null;
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
     public void recoverTree(TreeNode root) {
         helper(root);
 
-        
-        int temp = first.val;
-        first.val = second.val;
-        second.val = temp;
+        if(first != null && second != null){
+            int temp = first.val;
+            first.val = second.val;
+            second.val = temp;
+        } 
     }
-    
+
     public void helper(TreeNode root){
-        if(root == null)
+        if(root == null) 
             return;
-        
+
         helper(root.left);
 
         if(prev != null){
-            if(root.val < prev.val){
+            if(prev.val > root.val){
                 if(first == null){
                     first = prev;
                 }
                 second = root;
             }
         }
+
         prev = root;
-       
 
         helper(root.right);
     }
-
 }
