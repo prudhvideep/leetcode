0b108757ef87e5d8c872d0f3c8bc9f3b963ae92b
Prudhvi <prudhvideep1996@gmail.com>
2024-08-18 16:47:57 -0700

    Time: 78 ms (17.05%), Space: 45.4 MB (68.78%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -6,21 +6,23 @@ class Solution {
         st.push(num.charAt(0));
 
         for(int i = 1;i < num.length();i++){
-            while(!st.isEmpty() && (k > 0) && ((st.peek() - '0') > (num.charAt(i)-'0'))){
+            while(!st.isEmpty() && (k > 0) && (st.peek() > num.charAt(i))){
                 st.pop();
                 k--;
             }
             st.push(num.charAt(i));
         }
+
+        while(k-- > 0 && !st.isEmpty()){
+          st.pop();
+        }
+
         while(!st.isEmpty()){
             sb.insert(0,st.peek());
             st.pop();
         }
 
-        while(k-- > 0 && sb.length() > 0){
-          int n = sb.length();
-          sb.deleteCharAt(n-1);
-        }
+        
         while(sb.length() > 1 && sb.charAt(0) == '0'){
           sb.deleteCharAt(0);
         }
