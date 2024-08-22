1af87871e05fec2e490897146120bb96c487f982
Prudhvi <prudhvideep1996@gmail.com>
2024-08-21 18:38:41 -0700

    Time: 0 ms (100%), Space: 40.3 MB (67.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findComplement(int num) {
-        int n = num, mask = 0;
-        
+        int mask = 0, n = num;
+
         while(n > 0){
             mask = (mask << 1) | 1;
             n = n >> 1;
         }
-        
-    return (~num) & mask;
+
+        return  (~num) & mask;
     }
 }
