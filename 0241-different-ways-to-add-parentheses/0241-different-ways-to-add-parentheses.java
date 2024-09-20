546e834876a89d39468d6b923d43e7be57a8cec9
Prudhvi <prudhvideep1996@gmail.com>
2024-09-19 18:33:54 -0700

    Time: 1 ms (99.68%), Space: 41.6 MB (90.36%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,7 +1,8 @@
 class Solution {
-
+  List <Integer> [][] dp;
   public List<Integer> diffWaysToCompute(String expression) {
-
+    int n = expression.length();
+    dp = new ArrayList[n][n];
     List<Integer> list = solveExpression(0, expression.length() - 1, expression);
     return list;
   }
@@ -10,11 +11,23 @@ class Solution {
     List<Integer> res = new ArrayList<>();
     boolean hasOper = false;
 
+    if(dp[s][e] != null)
+      return dp[s][e]; 
+
     for (int i = s; i <= e; i++) {
       if (!Character.isDigit(exp.charAt(i))) {
         hasOper = true;
-        List<Integer> op1 = solveExpression(s, i - 1, exp);
-        List<Integer> op2 = solveExpression(i + 1, e, exp);
+        List<Integer> op1;
+        if(dp[s][i-1] == null){
+           dp[s][i-1] = solveExpression(s, i - 1, exp);
+        }
+        op1 = dp[s][i-1];
+        
+        List<Integer> op2;
+         if(dp[i+1][e] == null){
+           dp[i+1][e] = solveExpression(i + 1, e, exp);
+        }
+        op2 = dp[i+1][e];
 
         String oper = exp.substring(i, i + 1);
 
