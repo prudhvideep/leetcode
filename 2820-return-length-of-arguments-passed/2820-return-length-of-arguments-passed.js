4b004b97713279937a3c5222997fd6b76d2ede95
Prudhvi <prudhvideep1996@gmail.com>
2024-12-22 15:35:21 -0700

    Time: 44 ms (90.07%), Space: 48.9 MB (48.91%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let argArr = [...args]
+
+    return argArr.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
