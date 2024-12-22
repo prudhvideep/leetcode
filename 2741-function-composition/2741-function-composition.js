4015b15326167d803638afa50e0d2d643e8b7a59
Prudhvi <prudhvideep1996@gmail.com>
2024-12-22 15:34:23 -0700

    Time: 65 ms (49.87%), Space: 49.8 MB (83.83%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -5,16 +5,9 @@
 var compose = function (functions) {
 
   return function (x) {
-    let res = x
-    for (let i = functions.length - 1; i >= 0; i--) {
-      let fn = functions[i]
+    let revFns = [...functions].reverse()
 
-
-      res = fn(res)
-
-    }
-
-    return res
+    return revFns.reduce((acc,fun) => fun(acc),x)
   }
 };
 
