7822df2eddc6e769b460182033f405a8b1799f7c
Prudhvi <prudhvideep1996@gmail.com>
2025-02-01 01:31:15 -0700

    Time: 0 ms (100%), Space: 4.5 MB (97%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.go
+++ 0684-redundant-connection/0684-redundant-connection.go
@@ -1,13 +1,15 @@
-func find(parent []int, node int) int {
+func find(node int) int {
 	if parent[node] == node {
 		return node
 	}
 
-	return find(parent, parent[node])
+	return find(parent[node])
 }
 
+var parent []int
+
 func findRedundantConnection(edges [][]int) []int {
-	parent := make([]int, len(edges)+1)
+	parent = make([]int, len(edges)+1)
 	for i := range parent {
 		parent[i] = i
 	}
@@ -17,8 +19,7 @@ func findRedundantConnection(edges [][]int) []int {
 	for _, edge := range edges {
 		u, v := edge[0], edge[1]
 
-		pu, pv := find(parent, parent[u]), find(parent, parent[v])
-		// fmt.Printf("u , %d, v, %d, pu, %d, pv, %d\n", u, v, pu, pv)
+		pu, pv := find(parent[u]), find(parent[v])
 
 		if pu == pv {
 			redEdge[0] = u
