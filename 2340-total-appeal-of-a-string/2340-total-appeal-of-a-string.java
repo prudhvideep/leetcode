ef144053e1a8af9f151e52687a20d3d5f6bad3eb
Prudhvi <prudhvideep1996@gmail.com>
2025-05-10 15:54:35 -0700

    Time: 6 ms (88.89%), Space: 45.5 MB (50.83%) - LeetHub

---
+++ 2340-total-appeal-of-a-string/2340-total-appeal-of-a-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long appealSum(String s) {
+        long curApp = 0, totApp = 0;
+        int []pos = new int[26];
+
+        for(int i = 0;i < s.length();i++){
+          int id = s.charAt(i) -'a';
+          
+          curApp += i - pos[id] +1;
+          pos[id] = i+1;
+
+          totApp += curApp;
+        }
+
+        return totApp;
+    }
+}
